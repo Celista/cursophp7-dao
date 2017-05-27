f043afb714e473f2b0690d5707d237f3ef722fda
celista <lcelista@gmail.com>
2017-05-27 16:15:56 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -4,13 +4,24 @@ require_once("config.php");
 
 //$sql = new Sql();
 
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(1);
+//echo $root;
 
-//echo json_encode($usuarios);
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$root = new Usuario();
-$root->loadbyId(1);
 
-echo $root;
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("jose","423423");
+
+echo $usuario;
 
 ?>
